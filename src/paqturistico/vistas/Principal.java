ec5488a992c228c8dc04635793d0747943aceef1
Lucio Di Sanzo <luciodisanzo@gmail.com>
2021-11-08 20:53:15 -0300

    Modelo de vistaArmarPaquete

--- src/paqturistico/vistas/Principal.java
+++ src/paqturistico/vistas/Principal.java
@@ -43,6 +43,7 @@ public class Principal extends javax.swing.JFrame {
         jMenu4 = new javax.swing.JMenu();
         jMenu5 = new javax.swing.JMenu();
         jMenu6 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         jMenu1.setText("jMenu1");
 
@@ -127,6 +128,15 @@ public class Principal extends javax.swing.JFrame {
         jMenuBar1.add(jMenu5);
 
         jMenu6.setText("Paquete");
+
+        jMenuItem1.setText("Armar paquete");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu6.add(jMenuItem1);
+
         jMenuBar1.add(jMenu6);
 
         setJMenuBar(jMenuBar1);
@@ -220,6 +230,17 @@ public class Principal extends javax.swing.JFrame {
         escritorio.moveToFront(vActualizarDestino);
     }//GEN-LAST:event_miActualizarDestinoActionPerformed
 
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        VistaArmarPaquete vap = new VistaArmarPaquete();
+        vap.setVisible(true);
+        
+        escritorio.removeAll();
+        escritorio.repaint();
+        escritorio.add(vap);
+        escritorio.moveToFront(vap);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -264,6 +285,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu5;
     private javax.swing.JMenu jMenu6;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jmActualizarCliente;
     private javax.swing.JMenuItem jmAgregarCliente;
     private javax.swing.JMenuItem jmBorrarCliente;
